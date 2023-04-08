869a9e0a3ec7b40433f48af5b97e5b3d56459518
Ishi <ishikawatsubasa@Macintosh-2.local>
2023-04-09 03:53:02 +0900

    up

---
+++ 220~/227/B.py
@@ -0,0 +1,16 @@
+n = int(input())
+s = list(map(int, input().split()))
+
+judge = None
+ans = 0
+for i in s:
+    judge = 0
+    for j in range(1, 1000):
+        for k in range(1, 1000):
+            if int(4 * j * k + 3 * j + 3 * k) == i:
+                judge = 1
+
+    if judge == 1:
+        ans += 1
+
+print(n - ans)
